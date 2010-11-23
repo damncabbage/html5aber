5d1c412e9e1fbc2c4ae2ba5c21acced8f0806ffd
Rob Howard <damncabbage@gmail.com>
2010-11-24 03:28:22 +1100

    Added debugging code to check swing effects on desktop. Audio is still jittery; looking at switching to setInterval, as setTimeout events may be building up, regardless of clearTimeout() calls.

--- restruct.php
+++ restruct.php
@@ -53,7 +53,7 @@
 
 </head>
 <body>
-
+<div id="s" style="width: 50px; height: 50px; background: #afa; position: absolute; bottom: 0;">Here</div>
 	<div id="splash">HTML5aber</div>
 
 	<div id="bg">
@@ -64,117 +64,130 @@
 	<script src="/js/jquery.jswipe-0.1.2.js"></script>
 	<script>
 		$(document).ready(function(){
-			var sfxIdle = null;
-			var sfxOn = null;
-			var sfxOff = null;
-			var sfxSwings = [];
-			var sfxStrikes = [];
-			var playIdle = function(){};
-			var playInterrupt = false;
-			
-			$('#splash').click(function(){
-				sfxIdle = new Audio();
-				sfxIdle.src = '/audio/idle0.mp3';
-				sfxIdle.load();
 
-				sfxOn = new Audio();
-				sfxOn.src = '/audio/on0.mp3';
-				sfxOn.load();
+			var audio = {
+				idle: {
+					src: '/audio/idle.mp3',
+					looping: true, length: 3000,
+					elt: null
+				},
+				on: {
+					src: '/audio/on0.mp3',
+					looping: false, length: 1800,
+					elt: null
+				},
+				off: {
+					src: '/audio/off0.mp3',
+					looping: false, length: 1500,
+					elt: null
+				},
+				swings: [
+					{ src: '/audio/swing_edit1.mp3', looping: false,
+					  length: 1000, elt: null },
+					{ src: '/audio/swing_edit2.mp3', looping: false,
+					  length: 1000, elt: null }
+				],
+				strikes: [
+					{ src: '/audio/strike1.mp3', looping: false,
+					  length: 1000, elt: null },
+					{ src: '/audio/strike2.mp3', looping: false,
+					  length: 1000, elt: null }
+				]
+			};
 
-				sfxOff = new Audio();
-				sfxOff.src = '/audio/off0.mp3';
-				sfxOff.load();
+			var currentAudio = null;
+			var currentAudioLoopTimeout = null;
 
-				sfxSwings[0] = new Audio();
-				sfxSwings[0].src = '/audio/swing0.mp3';
-				sfxSwings[0].load();
-				sfxSwings[1] = new Audio();
-				sfxSwings[1].src = '/audio/swing7.mp3';
-				sfxSwings[1].load();
+			var loadAudioElements = function(types) {
+				var loadElement = function(section){
+					section.elt = new Audio();
+					section.elt.src = section.src;
+					section.elt.load();
+				};
+				loadElement(types.idle);
+				loadElement(types.on);
+				loadElement(types.off);
+				for (ii in types.swings) {
+					loadElement(types.swings[ii]);
+				}
+				for (ii in types.strikes) {
+					loadElement(types.strikes[ii]);
+				}
+			};
 
-				sfxStrikes[0] = new Audio();
-				sfxStrikes[0].src = '/audio/strike1.mp3';
-				sfxStrikes[0].load();
-				sfxStrikes[1] = new Audio();
-				sfxStrikes[1].src = '/audio/strike2.mp3';
-				sfxStrikes[1].load();
-
-				sfxOn.addEventListener('play', function(){
-
-					var playIdle = function(){
-						var rewind = function(){
-							window.setTimeout(function(){
-								if (!playInterrupt) {
-									sfxIdle.currentTime = 0;
-									rewind();
-								}
-							}, 2000);
-						};
-						sfxIdle.play();
-						playInterrupt = false;
-						rewind();
-					};
-
-					window.setTimeout(function(){
-						/*sfxOn.removeEventListener('play', sfxOnEvent);*/
-						/*
-						sfxOn.src = '/audio/idle0.mp3';
-						sfxOn.load();
-						sfxOn.addEventListener('play',function(){
-							
-						});
-						sfxOn.play();
-						*/
-						playIdle();
-					}, 1500);
-
-					window.ondevicemotion = function(event){
-						if (event.accelerationIncludingGravity.x > 3 ||
-							event.accelerationIncludingGravity.y > 3)
-						{
-							playInterrupt = true;
-							if (event.accelerationIncludingGravity.x > 7.6 ||
-								event.accelerationIncludingGravity.y > 7.6)
-							{
-								var randPlayerIdx = Math.floor(Math.random()*2);
-								sfxStrikes[randPlayerIdx].addEventListener('ended', playIdle);
-								sfxStrikes[randPlayerIdx].play();
-							} else {
-								var randPlayerIdx = Math.floor(Math.random()*2);
-								sfxSwings[randPlayerIdx].addEventListener('ended', playIdle);
-								sfxSwings[randPlayerIdx].play();
-							}
-						}
+			var playAudio = function(item, postItem) {
+				if (currentAudio !== null) {
+					if (currentAudioLoopTimeout !== undefined) {
+						window.clearTimeout(currentAudioLoopTimeout);
 					}
-				});
+					currentAudio.removeEventListener('ended', playAudio);
+					currentAudio.pause();
+					if (currentAudio.currentTime) {
+						currentAudio.currentTime = 0;
+					}
+				}
+
+				currentAudio = item.elt;
+				if (item.looping) {
+					var rewind = function(){
+						if (item.elt.src == currentAudio.src) {
+							window.setTimeout(function(){
+								item.elt.currentTime = 0.1; // 100ms from beginning, to skip fade-in.
+								rewind();
+							}, item.length);
+						}
+					};
+					item.elt.play();
+					rewind();
+				} else {
+					item.elt.play();
+				}
+
+				if (postItem !== undefined) {
+					// Set up an event to follow it.
+					item.elt.addEventListener('ended', function(){playAudio(postItem)});
+				}
+			};
+
+
+			$('#splash').click(function(){
+				loadAudioElements(audio);
 
 				var splash = $(this);
-				sfxOn.addEventListener('loadeddata',function(){
+				/* TODO: Set up loading spinner */
+				audio.idle.elt.addEventListener('loadeddata',function(){
+					/* TODO: Show instructions briefly. */
 					splash.hide();
 				});
-			});
 
+			}); /* splash.click */
+
+
+			var swingDetection = function(event){
+				/* Woom, woom... Woom. (Slower swings) */
+				if (event.accelerationIncludingGravity.x > 3 ||
+					event.accelerationIncludingGravity.y > 3)
+				{
+					/* Really hacky, not indicative of a "strike" so much as a quicker swing. */
+					if (event.accelerationIncludingGravity.x > 7.6 ||
+						event.accelerationIncludingGravity.y > 7.6)
+					{
+						var randPlayerIdx = Math.floor(Math.random() * audio.strikes.length);
+						playAudio(audio.strikes[randPlayerIdx], audio.idle);
+					} else {
+						var randPlayerIdx = Math.floor(Math.random() * audio.swings.length);
+						playAudio(audio.swings[randPlayerIdx], audio.idle);
+					}
+				}
+			};
 
 			var beam = $('#beam');
 			$('#bg').swipe({
 				swipeUp:function(){
 					if (!beam.hasClass('extended')) {
 						beam.addClass('extended');
-						sfxOn.play();
-						/*
-
-						if (sfxOn.currentTime > 0) {
-							sfxOn.load();
-							sfxOn.currentTime = 0;
-						}
-						sfxOn.addEventListener('play',function(){
-							window.setTimeout(function(){
-								sfxOn.pause();
-								sfxOff.load();
-								sfxOff.play();
-							}, 1300);
-						});
-						sfxOn.play();*/
+						playAudio(audio.on, audio.idle);
+						window.ondevicemotion = swingDetection;
 					}
 				},
 			});
@@ -182,36 +195,33 @@
 				swipeDown:function(){
 					if (beam.hasClass('extended')) {
 						beam.removeClass('extended');
-						sfxIdle.pause();
 						window.ondevicemotion = null;
-						sfxOff.play();
+						playAudio(audio.off);
 					}
 				}
 			});
-		});
 
-							/*
-							var rewind = function(){
-								window.setTimeout(function(){
-									sfxOn.currentTime = 0;
-									rewind();
-								}, 1000);
-							};
-							rewind();
-							*/
-							/*
-							window.setTimeout(rewind, 700);
-							sfxIdle.load({url: '/audio/on0.mp3'});
-							sfxIdle.play();
-							*/
-							/* Cheap way of looping */
-							/*
-							sfxIdle.addEventListener('ended',function(){
-								this.pause();
-								this.currentTime = 0;
-								this.play();
+				$('#bg').click(function(){
+						if (!beam.hasClass('extended')) {
+							beam.addClass('extended');
+							playAudio(audio.on, audio.idle);
+							window.ondevicemotion = swingDetection;
+
+							$('#s').click(function(){
+								var randPlayerIdx = Math.floor(Math.random() * audio.swings.length);
+								playAudio(audio.swings[randPlayerIdx], audio.idle);
+
 							});
-							*/
+						} else {
+							beam.removeClass('extended');
+							window.ondevicemotion = null;
+							playAudio(audio.off);
+						}
+				});
+
+
+		}); /* document.ready */
+
 	</script>
 </body>
 </html>
