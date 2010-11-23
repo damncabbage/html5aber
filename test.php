66f2661eca72ea85ad37641becb550765ddd5313
Rob Howard <rhoward@vps.robhoward.id.au>
2010-11-23 19:39:34 +1100

    Initial commit.

---
+++ test.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HTML5aber</title>
+	<meta name="language" content="en" /> 
+	<meta name="viewport" content="width=320; user-scalable=0; initial-scale=1.0; maximum-scale=1.0;" /> 
+	<meta name="apple-mobile-web-app-capable" content="yes" /> 
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+	<style>
+		body {
+			background: #000;
+			width: 320px;
+		}
+		#beam {
+			width: 100%;
+			height: 0;
+			background: -webkit-gradient(
+				linear, left top, left bottom,
+				color-stop(0%,#2bdcff), color-stop(20%,#FFFFFF),
+				color-stop(80%,#FFFFFF), color-stop(100%,#2bdcff)
+			);
+		}
+	</style>
+
+</head>
+<body>
+
+	<div id="beam"></div>
+	
+
+
+	<script src="/js/jquery-1.4.4.min.js"></script>
+	<script>
+
+	</script>
+</body>
+</html>
